ee8626146db2fc0ef9f0c23b3a8506e8444bfab1
zsm <zsm@zheng.com>
2021-09-25 16:21:31 +0800

    first commit

---
+++ src/main/java/com/user/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.user.git;
+
+/**
+ * @author zsm
+ * @date 2021-09-25-16:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
